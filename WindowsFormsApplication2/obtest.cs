57f714727b83a05a8e0f97eb93fdc545c716d93c
adiesha <adiesha@gmail.com>
2014-09-22 10:24:18 +0530

    jn

---
+++ WindowsFormsApplication2/obtest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApplication2
+{
+    class obtest
+    {
+        public void testmethod(Button b)
+        {
+            b.Text = "jsjfsdf";
+        }
+    }
+}
